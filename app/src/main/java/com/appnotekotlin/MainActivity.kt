212393b8da473cd36e8b98512e8703d0d78cdbca
Reza <karimnia.reza70@yahoo.com>
2022-02-13 13:30:22 +0330

    add dataBinding

--- app/src/main/java/com/appnotekotlin/MainActivity.kt
+++ app/src/main/java/com/appnotekotlin/MainActivity.kt
@@ -2,10 +2,14 @@ package com.appnotekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.appnotekotlin.databinding.ActivityMainBinding
+import com.appnotekotlin.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
